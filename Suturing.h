272bd3d1bd0d4771eda699f6fb267ddf3993d366
Giannos Siokos <123774295+Giannossk@users.noreply.github.com>
2025-02-01 15:28:32 +0200

    Update Suturing.h

--- Suturing.h
+++ Suturing.h
@@ -4,41 +4,30 @@
 #include <sofa/core/ObjectFactory.h>
 #include <sofa/core/BaseObject.h>
 #include <sofa/core/objectmodel/Data.h>
-
-// Include the performer and spring components:
-#include <sofa/gui/component/performer/SuturePointPerformer.h>
 #include <sofa/component/solidmechanics/spring/SpringForceField.h>
+#include <sofa/component/statecontainer/MechanicalObject.h>
+#include <sofa/defaulttype/Vec3dTypes.h>
 
-// Forward-declare the simulation event we'll handle
-#include <sofa/simulation/EndStepEvent.h>
-
-namespace sofa
-{
-namespace component
-{
-namespace suturing
-{
+namespace sofa {
+namespace component {
+namespace suturing {
 
 class Suturing : public core::objectmodel::BaseObject
 {
 public:
     SOFA_CLASS(Suturing, core::objectmodel::BaseObject);
 
-    // Data fields to hold pointers to the performer and the spring.
-    Data< core::objectmodel::SPtr< gui::component::performer::SuturePointPerformer > > d_suturePerformer;
+    Data< core::objectmodel::SPtr< component::statecontainer::MechanicalObject<defaulttype::Vec3d> > > d_tissueObject;
     Data< core::objectmodel::SPtr< solidmechanics::spring::SpringForceField > > d_sutureSpring;
+    Data<bool> d_autoSuture;
 
     Suturing();
     virtual ~Suturing();
 
-    // Standard initialization function (called once after construction)
     void init() override;
-
-    // Override event handler to update suturing each simulation step.
     void handleEvent(core::objectmodel::Event* event) override;
 
 protected:
-    /// Update suture spring based on picked points.
     void updateSuture(double dt);
 };
 
